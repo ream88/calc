d55da22204bec14d403e291a28fdd4ef6ac75b78
Mario Uher <uher.mario@gmail.com>
2020-11-11 15:16:19 +0100

    Implement tests

--- lib/calc.ex
+++ lib/calc.ex
@@ -1,18 +1,2 @@
 defmodule Calc do
-  @moduledoc """
-  Documentation for `Calc`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Calc.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -1,8 +1,41 @@
 defmodule CalcTest do
   use ExUnit.Case
-  doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
+  test "is a process" do
+    calc = spawn(&Calc.start/0)
+    assert Process.alive?(calc)
+
+    Process.exit(calc, :exit)
+    refute Process.alive?(calc)
+  end
+
+  test "starts with zero" do
+    calc = spawn(&Calc.start/0)
+
+    send(calc, {:print, self()})
+    assert_receive 0
+  end
+
+  test "adding" do
+    calc = spawn(&Calc.start/0)
+
+    send(calc, 1)
+    send(calc, 1)
+
+    send(calc, {:print, self()})
+    assert_receive 2
+  end
+
+  test "sending anything else does not crash the calc" do
+    calc = spawn(&Calc.start/0)
+
+    send(calc, :hello)
+    send(calc, nil)
+    send(calc, [])
+
+    send(calc, {:print, self()})
+    assert_receive 0
+
+    assert Process.alive?(calc)
   end
 end
